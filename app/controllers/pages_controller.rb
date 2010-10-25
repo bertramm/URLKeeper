25c028b5798470f5ef7614dbb92804fd2e35c06d
Matthew Bertram <matt@unitkeeper.com>
2010-10-25 07:22:54 -0700

    Added bookmarklets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,27 +10,27 @@ class PagesController < ApplicationController
      if url != "http://pure-ocean-97.heroku.com/"
        
      
-     #url = CGI::escape("Test URL")
-     #user = CGI::escape("Test User")
-     #desc = CGI::escape("Test desc")
-     http = Net::HTTP.new('spreadsheets.google.com',443)
-     http.use_ssl = true
-     path = '/formResponse?formkey=dHkxU3BVRTZlZXB1X1JET2l5WWVUQXc6MQ&ifq'
-     # POST request -> logging in
+       #url = CGI::escape("Test URL")
+       #user = CGI::escape("Test User")
+       #desc = CGI::escape("Test desc")
+       http = Net::HTTP.new('spreadsheets.google.com',443)
+       http.use_ssl = true
+       path = '/formResponse?formkey=dHkxU3BVRTZlZXB1X1JET2l5WWVUQXc6MQ&ifq'
+       # POST request -> logging in
 
-     data = 'entry.0.single='+url+'&entry.1.single='+desc+'&entry.2.single='+user+'&pageNumber=0&    backupCache=&submit=Submit'
+       data = 'entry.0.single='+url+'&entry.1.single='+desc+'&entry.2.single='+user+'&pageNumber=0&    backupCache=&submit=Submit'
 
-     print data
-     headers = { "Content-type" => "application/x-www-form-urlencoded", "Accept"=> "text/plain,application'xhtml+xml,application/xml;q=0.9,*/*;q=0.8 "}
+       print data
+       headers = { "Content-type" => "application/x-www-form-urlencoded", "Accept"=> "text/plain,application'xhtml+xml,application/xml;q=0.9,*/*;q=0.8 "}
 
-     resp, data = http.post(path, data, headers)
+       resp, data = http.post(path, data, headers)
 
 
-     # Output on the screen -> we should get either a 302 redirect (after a successful login) or an error page
-     #puts 'Code = ' + resp.code
-     #puts 'Message = ' + resp.message
-     #resp.each {|key, val| puts key + ' = ' + val}
-     #puts data
+       # Output on the screen -> we should get either a 302 redirect (after a successful login) or an error page
+       #puts 'Code = ' + resp.code
+       #puts 'Message = ' + resp.message
+       #resp.each {|key, val| puts key + ' = ' + val}
+       #puts data
 
      end
   end
