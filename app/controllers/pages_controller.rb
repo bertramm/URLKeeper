1f89337c8404ffc7ed217e062825a518923d0e93
Matthew Bertram <matt@unitkeeper.com>
2010-10-25 08:10:51 -0700

    Tweeked a few things

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,30 +3,36 @@ require 'net/https'
 
 class PagesController < ApplicationController
   def url
+     #Define Path to Google Form
+     googleURL = 'spreadsheets.google.com'
+     #You can find this in the source of your google form
+     formPath = '/formResponse?formkey=dHkxU3BVRTZlZXB1X1JET2l5WWVUQXc6MQ&ifq'
+     bookmarkletLocation = "http://pure-ocean-97.heroku.com/"
+
      url = params['url'].nil?  ? "" : CGI::escape(params['url'])
      user = params['user'].nil?  ? "" : CGI::escape(params['user'])
      desc = params['desc'].nil?  ? "" : CGI::escape(params['desc'])
      
-     if url != CGI::escape("http://pure-ocean-97.heroku.com/")
+     if url != CGI::escape(bookmarkletLocation)
        
      
        #url = CGI::escape("Test URL")
        #user = CGI::escape("Test User")
        #desc = CGI::escape("Test desc")
-       http = Net::HTTP.new('spreadsheets.google.com',443)
+
+       http = Net::HTTP.new('googleURL',443)
        http.use_ssl = true
-       path = '/formResponse?formkey=dHkxU3BVRTZlZXB1X1JET2l5WWVUQXc6MQ&ifq'
-       # POST request -> logging in
 
-       data = 'entry.0.single='+url+'&entry.1.single='+desc+'&entry.2.single='+user+'&pageNumber=0&    backupCache=&submit=Submit'
+       
+       data = 'entry.0.single='+url+'&entry.1.single='+desc+
+              '&entry.2.single='+user+'&pageNumber=0&backupCache=&submit=Submit'
 
-       print data
-       headers = { "Content-type" => "application/x-www-form-urlencoded", "Accept"=> "text/plain,application'xhtml+xml,application/xml;q=0.9,*/*;q=0.8 "}
+       headers = { "Content-type" => "application/x-www-form-urlencoded", 
+                   "Accept"=> "text/plain,application'xhtml+xml,application/xml;q=0.9,*/*;q=0.8 "}
 
-       resp, data = http.post(path, data, headers)
+       # POST request
+       resp, data = http.post(formPath, data, headers)
 
-
-       # Output on the screen -> we should get either a 302 redirect (after a successful login) or an error page
        #puts 'Code = ' + resp.code
        #puts 'Message = ' + resp.message
        #resp.each {|key, val| puts key + ' = ' + val}
