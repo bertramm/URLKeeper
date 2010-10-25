26224f40490459c2cb3254d0fae5407bc106c15a
Matthew Bertram <matt@unitkeeper.com>
2010-10-25 05:57:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urlbookmarker::Application.config.secret_token = '3641be05504d697298cd4621e1b6096ab2cc737d73aed01ed984846e833fe7a0ab52ef889552de49fdc6122d33b75a2db5c5afea031ed5c003e7b66a4058fc43'
